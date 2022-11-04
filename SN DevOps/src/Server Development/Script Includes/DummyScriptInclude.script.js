01ca998d54e6fafdb1bd8f66608e14c96c5f1224
shuangjianyang <shuangjian.yang@ray-soft.com>
2022-11-04 17:11:02 +0800

    added feature1

--- SN DevOps/src/Server Development/Script Includes/DummyScriptInclude.script.js
+++ SN DevOps/src/Server Development/Script Includes/DummyScriptInclude.script.js
@@ -1,7 +1,9 @@
 var DummyScriptInclude = Class.create();
 DummyScriptInclude.prototype = {
     initialize: function() {
+        //developer added code for feature1
     },
+    
 
     type: 'DummyScriptInclude'
 };
